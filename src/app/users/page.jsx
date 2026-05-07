cefa1e0b87ed315c23e3d120d404c019006c072f
juhairulislam <juhairulislam2018@gmail.com>
2026-05-07 12:22:56 +0600

    get user data and show

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { getUsers } from '../lib/data';
 
-const UsersPage = () => {
+const UsersPage =async () => {
+
+    const users = await getUsers()
     return (
         <div>
-            <h2>User Management:</h2>
+            <h2>User Management: {users.length}</h2>
         </div>
     );
 };
