d709d5f3d3d3f4f0afd371ec2a87b9cb44b1e314
juhairulislam <juhairulislam2018@gmail.com>
2026-05-07 21:20:14 +0600

    set modal in users page

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { getUsers } from '../lib/data';
 import UsersTable from '../components/UsersTable';
 import { deleteUser } from '../lib/actions';
+import AddUserModal from '../components/AddUserModal';
 
 const UsersPage =async () => {
 
     const users = await getUsers()
     return (
-        <div className='p-15 space-y-4'>
+        <div className='p-15 '>
+            <div className='space-y-4'>
             <h2 className='text-2xl font-bold text-center'>User Management: {users.length}</h2>
+<AddUserModal></AddUserModal>
+
+            </div>
             <UsersTable users={users} deleteUserAction = {deleteUser}></UsersTable>
         </div>
     );
