bb656a73fe5dff5498ba43011325603b8bcd04f1
juhairulislam <juhairulislam2018@gmail.com>
2026-05-07 20:02:48 +0600

    implement deleteUser method

---
+++ src/app/lib/actions.js
@@ -0,0 +1,14 @@
+const deleteUser = async (userId) => {
+    'use server';
+
+    const res = await fetch(`http://localhost:5000/users/${userId}` ,
+        {
+            method:'DELETE'
+        }
+    );
+    const data = await res.json();
+
+    // TODO: 
+    return data;
+
+}
