138bbb425f579b86b6a403c6723d998dd5052be8
juhairulislam <juhairulislam2018@gmail.com>
2026-05-07 14:55:09 +0600

    get user details in details page

--- src/app/users/[userId]/page.jsx
+++ src/app/users/[userId]/page.jsx
@@ -1,6 +1,14 @@
+import { getUserById } from '@/app/lib/data';
 import React from 'react';
 
-const UserDetailsPage = () => {
+const UserDetailsPage =async ({params}) => {
+
+    const {userId} = await params ;
+
+    const user = await getUserById(userId) 
+
+    console.log(user)
+
     return (
         <div>
             <h1>THis is user details page</h1>
