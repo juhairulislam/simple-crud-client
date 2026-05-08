ab0f263fb399268f1f7eb740aa7149f569045236
juhairulislam <juhairulislam2018@gmail.com>
2026-05-08 10:54:38 +0600

    user route padding responsive

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -8,7 +8,7 @@ const UsersPage =async () => {
 
     const users = await getUsers()
     return (
-        <div className='p-15 '>
+        <div className='p-5 md:p-15 '>
             <div className='space-y-4 flex justify-between'>
             <h2 className='text-2xl font-bold text-center'>User Management: {users.length}</h2>
 <AddUserModal createUserAction={createUser}></AddUserModal>
