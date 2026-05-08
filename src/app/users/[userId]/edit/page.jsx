26db3b424e92fd247bcac3d6191cfc1c7a537408
juhairulislam <juhairulislam2018@gmail.com>
2026-05-08 11:05:21 +0600

    add edit form

--- src/app/users/[userId]/edit/page.jsx
+++ src/app/users/[userId]/edit/page.jsx
@@ -1,4 +1,5 @@
 import { getUserById } from '@/app/lib/data';
+import { Button, Input, Label, TextField } from '@heroui/react';
 import React from 'react';
 
 const UserEditPage =async ({params}) => {
@@ -11,6 +12,29 @@ const UserEditPage =async ({params}) => {
     return (
         <div>
             <h2>Editing User: {user.name} </h2>
+
+            <div className='w-1/3 mx-auto'>
+                 <form  className="flex flex-col gap-4">
+                                  <TextField className="w-full" name="name" type="text">
+                                    <Label>Name</Label>
+                                    <Input placeholder="Enter your name" />
+                                  </TextField>
+                                  <TextField className="w-full" name="email" type="email">
+                                    <Label>Email</Label>
+                                    <Input placeholder="Enter your email" />
+                                  </TextField>
+                                  <TextField className="w-full" name="role" type="tel">
+                                    <Label>Role</Label>
+                                    <Input placeholder="Enter user role" />
+                                  </TextField>
+                
+                              <Button slot="close" variant="secondary">
+                                Cancel
+                              </Button>
+                              <Button type='submit' slot="close">Add User</Button>
+                               
+                                </form>
+            </div>
         </div>
     );
 };
